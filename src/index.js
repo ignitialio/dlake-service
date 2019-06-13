38585b747b5438d0edd7a94b20597133c9840a20
Victor Andritoiu <devandrito@gmail.com>
2019-06-13 22:29:19 +0200

    change main function naming rule

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import DLake from './components/DLake.vue'
 
-global.service_dlake = function(Vue) {
+global.iios_dlake = function(Vue) {
   // Warning: component name must be globally unique in your host app
   Vue.component('dlake', DLake)
 }
